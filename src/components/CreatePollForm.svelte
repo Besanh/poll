d1a248395c626ef64927f6e22405ffba2387d9bd
besanh <anhle3532@mail.com>
2022-06-26 00:10:20 +0700

    custom button component

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,5 +1,6 @@
 <script>
     import { createEventDispatcher } from "svelte";
+    import Button from "../../src/shared/Button.svelte";
 
     const dispatch = createEventDispatcher();
     let customFields = { question: "", answerA: "", answerB: "" };
@@ -11,18 +12,18 @@
 
 <form on:submit|preventDefault={submitHandler}>
     <div class="form-field">
-        <label for="question">Poll question:</label>
+        <label for="question">Question</label>
         <input type="text" id="question" bind:value={customFields.question} />
     </div>
     <div class="form-field">
-        <label for="answer-a">Answer A:</label>
-        <input type="text" id="answer-a" bind:value={customFields.answerA} />
+        <label for="answerA">Answer A</label>
+        <input type="text" id="answerA" bind:value={customFields.answerA} />
     </div>
     <div class="form-field">
-        <label for="answer-b">Answer B:</label>
-        <input type="text" id="anser-b" bind:value={customFields.answerB} />
+        <label for="answerB">Answer B</label>
+        <input type="text" id="answerB" bind:value={customFields.answerB} />
     </div>
-    <button type="submit">Send</button>
+    <Button type_btn="secondary" flat={true} inverse={true}>Add</Button>
 </form>
 
 <style>
---
+++ src/shared/Button.svelte
@@ -0,0 +1,43 @@
+<script>
+    let type_btn = "primary";
+    let flat = false;
+    let inverse = false;
+    $: class_name = `${type_btn}`;
+    export { type_btn, flat, inverse };
+</script>
+
+<button class={class_name} class:flat class:inverse>
+    <slot />
+</button>
+
+<style>
+    button {
+        border: 0;
+        cursor: pointer;
+        border-radius: 5px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+    }
+    .primary {
+        background: #4424b6;
+        color: white;
+    }
+    .secondary {
+        background: #45c496;
+        color: white;
+    }
+    .flat {
+        box-shadow: none;
+    }
+    .primary.inverse {
+        color: #4424b6;
+        background: white;
+        border: 1px solid #4424b6;
+    }
+    .secondary.inverse {
+        color: #45c496;
+        background: white;
+        border: 1px solid #45c496;
+    }
+</style>
