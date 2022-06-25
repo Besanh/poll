928557cd6d98748032e87426ce51f3448ce30001
besanh <anhle3532@mail.com>
2022-06-25 21:49:05 +0700

    build tab content - reuseable component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,30 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = [
+    { id: 1, name: "Current Polls" },
+    { id: 2, name: "Add New Poll" },
+  ];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    activeItem = e.detail.name;
+  };
 </script>
 
 <Header />
-<main />
+<main>
+  <!-- Tabs component dispatch event 'tabChange' to this file -->
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
+  {#if activeItem === "Current Polls"}
+    <p>Current polls here</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>New poll form</p>
+  {/if}
+</main>
 <Footer />
 
 <style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+    let activeItem;
+    let items;
+
+    export { activeItem, items };
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item (item.id)}
+            <li
+                on:click={() => {
+                    dispatch("tabChange", item);
+                }}
+            >
+                <div class:active={item.name === activeItem}>
+                    {item.name}
+                </div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        list-style-type: none;
+    }
+    li {
+        margin: 0 16px;
+        font-size: 18px;
+        color: #555;
+        cursor: pointer;
+    }
+    .active {
+        color: #d91b42;
+        border-bottom: 2px solid #d91b42;
+        padding-bottom: 8px;
+    }
+</style>
